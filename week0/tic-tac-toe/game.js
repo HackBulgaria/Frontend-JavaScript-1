da4d1501c09d80726b8f258b4d59cda84c39b958
Radoslav Georgiev <g2h.horde@gmail.com>
2014-05-27 15:00:28 +0300

    Removing game logic

--- week0/tic-tac-toe/game.js
+++ week0/tic-tac-toe/game.js
@@ -20,34 +20,20 @@ function gameLoop() {
               ["*", "*", "*"] ],
     xTurn = true,
     position = null;
-
-    var player1Name, player2Name;
-
-    player1Name = prompt("Enter name for player1>");
-    player2Name = prompt("Enter name for player2>");
-
   while(true) {
 
     console.log("This is the current state of the board:");
     printBoard(board);
 
     if(xTurn) {
-      console.log("Place for " + player1Name);
+      console.log("Place for x");
     } else {
-      console.log("Place for " + player2Name);
+      console.log("Place for y");
     }
 
     // this is blocking prompt
-    position = prompt("x y>").split(" ");
+    position = prompt("x y>");
     console.log(position);
-    var x = parseInt(position[0], 10);
-    var y = parseInt(position[1], 10);
-
-    var row = board[x-1];
-    row[y-1] = "X";
-    console.log(row);
-    board[x-1] = row;
-
     xTurn = !xTurn;
   }
 }
