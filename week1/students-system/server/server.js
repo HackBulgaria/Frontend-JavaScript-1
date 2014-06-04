46a0ba343fd4cd329e07a261b0b768a8e798b9f7
Radoslav Georgiev <g2h.horde@gmail.com>
2014-06-04 21:42:15 +0300

    Adding server for students-system

---
+++ week1/students-system/server/server.js
@@ -0,0 +1,38 @@
+"use strict";
+// trying to start this file but something's not working?
+// try npm install first
+
+var express = require('express');
+var app = express();
+
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
+var students = [{
+  "name" : "Daniel Taskoff",
+  "course" : "Frontend JavaScript"
+}, {
+  "name" : "Elena Jeleva",
+  "course" : "Programming 101"
+}, {
+  "name" : "Luboslava Dimitrova",
+  "course" : "Frontend JavaScript"
+}, {
+  "name" : "Anton Antonov",
+  "course" : "Core Java"
+}, {
+  "name" : "Nikola Dichev",
+  "course" : "Core Java"
+}];
+
+app.get('/students', function(req, res){
+  res.json(students);
+});
+
+
+var server = app.listen(3000, function() {
+    console.log('Listening on port %d', server.address().port);
+});
