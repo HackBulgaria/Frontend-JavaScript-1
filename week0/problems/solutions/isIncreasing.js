94e56ccfb64a87053e2e0ae3f185f8872e86ed01
dtaskoff <danko@abv.bg>
2014-06-08 20:07:06 +0300

    Solved isIncreasing problem

--- week0/problems/solutions/isIncreasing.js
+++ week0/problems/solutions/isIncreasing.js
@@ -1,1 +1,16 @@
-// solution here
+'use strict';
+
+
+var isIncreasing = function (seq) {
+    return seq.reduce(function (prev, curr) {
+        if (prev !== false && prev < curr) {
+            return curr;
+        }
+        return false;
+    }) && true;
+};
+
+console.log('1, 2, 3, 4, 5 is increasing? ' +
+    isIncreasing([1, 2, 3, 4, 5]));
+console.log('1, 1, 2, 3, 4 is increasing? ' +
+    isIncreasing([1, 1, 2, 3, 4]));
