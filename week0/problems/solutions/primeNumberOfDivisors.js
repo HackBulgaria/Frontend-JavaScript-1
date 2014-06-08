eae5cc5a3836a731b97405a413f098412ba0e3b2
dtaskoff <danko@abv.bg>
2014-06-08 17:56:10 +0300

    Solved primeNumberOfDivisors problem

--- week0/problems/solutions/primeNumberOfDivisors.js
+++ week0/problems/solutions/primeNumberOfDivisors.js
@@ -1,1 +1,34 @@
-// solution here
+'use strict';
+
+
+var range = function (from, to) {
+    var arr = [];
+    while (from <= to) {
+        arr.push(from);
+        from += 1;
+    }
+    return arr;
+};
+
+var getAllDivisorsOf = function (number) {
+    return range(1, number).filter(function (x) {
+        return Math.floor(number % x, 10) === 0;
+    });
+};
+
+var isPrime = function (number) {
+    // for is the best in this case
+    for (var i = 2; i <= Math.sqrt(number); i += 1) {
+        if (Math.floor(number % i) === 0) {
+            return false;
+        }
+    }
+    return number >= 2;
+};
+
+var primeNumberOfDivisors = function (number) {
+    return isPrime(getAllDivisorsOf(number).length);
+};
+
+console.log('10 has prime number of divisors? ' + primeNumberOfDivisors(10));
+console.log('101 has prime number of divisors? ' + primeNumberOfDivisors(101));
