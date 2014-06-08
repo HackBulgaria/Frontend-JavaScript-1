57ce81f2a03aa730ce568c4725b92b22144fbff1
dtaskoff <danko@abv.bg>
2014-06-08 19:51:50 +0300

    Solved listToNumber problem

--- week0/problems/solutions/listToNumber.js
+++ week0/problems/solutions/listToNumber.js
@@ -1,1 +1,9 @@
-// solution here
+'use strict';
+
+
+var listToNumber = function (list) {
+    return Math.floor(list.join(''));
+};
+
+console.log('-, 0, 3, 5, 1 to number is ' +
+    listToNumber(['-', 0, 3, 5, 1]));
