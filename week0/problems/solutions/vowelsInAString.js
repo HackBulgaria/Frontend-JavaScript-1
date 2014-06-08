146fcccd5cc54c6b8339eb271ba3098418445e60
dtaskoff <danko@abv.bg>
2014-06-08 19:36:24 +0300

    Solved vowelsInAString problem

--- week0/problems/solutions/vowelsInAString.js
+++ week0/problems/solutions/vowelsInAString.js
@@ -1,1 +1,13 @@
-// solution here
+'use strict';
+
+
+var vowelsInAString = function (str) {
+    var patt = /^[aeiouy]$/i;
+    return str.split('').filter(function (char) {
+        return patt.test(char);
+    }).length;
+};
+
+console.log('Theistareykjarbunga has ' +
+    vowelsInAString('Theistareykjarbunga') +
+    ' vowels in it');
