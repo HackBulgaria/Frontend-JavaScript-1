3ccadbbfd2b2691c89a78e49949f6d2292cccef9
dtaskoff <danko@abv.bg>
2014-06-08 17:49:22 +0300

    Solved isPrime problem

--- week0/problems/solutions/isPrime.js
+++ week0/problems/solutions/isPrime.js
@@ -1,1 +1,15 @@
-// solution here
+'use strict';
+
+
+var isPrime = function (number) {
+    // for is the best in this case
+    for (var i = 2; i <= Math.sqrt(number); i += 1) {
+        if (Math.floor(number % i) === 0) {
+            return false;
+        }
+    }
+    return number >= 2;
+};
+
+console.log('101 is a prime? ' + isPrime(101));
+console.log('1 is a prime? ' + isPrime(1));
