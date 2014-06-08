957cc715db54d995649fd0a543aea964ccd9cc2f
dtaskoff <danko@abv.bg>
2014-06-08 17:39:51 +0300

    Solved sumOfDivisors problem

--- week0/problems/solutions/sumOfDivisors.js
+++ week0/problems/solutions/sumOfDivisors.js
@@ -1,1 +1,26 @@
-// solution here
+'use strict';
+
+
+var range = function (from, to) {
+    var arr = [];
+    while (from <= to) {
+        arr.push(from);
+        from += 1;
+    }
+    return arr;
+};
+
+var getAllDivisorsOf = function (number) {
+    return range(1, number).filter(function (x) {
+        return Math.floor(number % x, 10) === 0;
+    });
+};
+
+var sumOfDivisors = function (number) {
+    var plus = function (a, b) {
+        return a + b;
+    };
+    return getAllDivisorsOf(Math.abs(number)).reduce(plus, 0);
+};
+
+console.log('sum of divisors of 10 is: ' + sumOfDivisors(10));
