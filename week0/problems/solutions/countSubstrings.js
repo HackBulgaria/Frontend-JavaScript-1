48617f3f3dfd67172911c6e945aa0406e285e25b
dtaskoff <danko@abv.bg>
2014-06-08 18:43:55 +0300

    Solved countSubstrings problem

--- week0/problems/solutions/countSubstrings.js
+++ week0/problems/solutions/countSubstrings.js
@@ -1,1 +1,14 @@
-// solution here
+'use strict';
+
+
+var countSubstrings = function (haystack, needle) {
+    return haystack.split(needle).length - 1;
+};
+
+console.log('haystack has ' +
+    countSubstrings('haystack', 'ays') +
+    ' ays\' in it');
+
+console.log('needle has ' +
+    countSubstrings('needle', 'e') +
+    ' es in it');
