2d3bf8d536cc52c4f9c4444b856cf03124ee73b4
dtaskoff <danko@abv.bg>
2014-06-08 20:10:36 +0300

    Solved isDecreasing problem

--- week0/problems/solutions/isDecreasing.js
+++ week0/problems/solutions/isDecreasing.js
@@ -1,1 +1,16 @@
-// solution here
+'use strict';
+
+
+var isDecreasing = function (seq) {
+    return seq.reduceRight(function (prev, curr) {
+        if (prev !== false && prev < curr) {
+            return curr;
+        }
+        return false;
+    }) && true;
+};
+
+console.log('is 8, 5, 3, 2, 1 decreasing? ' +
+    isDecreasing([8, 5, 3, 2, 1]));
+console.log('is 1, 2, 43, 5, 8 decreasing? ' +
+    isDecreasing([1, 2, 43, 5, 8]));
