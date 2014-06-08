4dec128c03bcec881a1315fd6e7b1fa958af5909
dtaskoff <danko@abv.bg>
2014-06-08 18:18:49 +0300

    Solved isIntPalindrome problem

---
+++ week0/problems/solutions/isIntPalindrome.js
@@ -0,0 +1,15 @@
+'use strict';
+
+
+var isIntPalindrome = function (number) {
+    return number
+        .toString()
+        .split('')
+        // checks if every item is equal to it's symmetric element in the array
+        .every(function (value, index, arr) {
+            return value === arr[arr.length - index - 1];
+        });
+};
+
+console.log('123454321 is a palindrome? ' + isIntPalindrome(123454321));
+console.log('12345 is a palindrome? ' + isIntPalindrome(12345));
