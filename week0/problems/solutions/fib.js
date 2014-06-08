9ebabf7dad83ebdfc64501d77c2bfc3c59a3b7e9
dtaskoff <danko@abv.bg>
2014-06-08 16:59:47 +0300

    Solved fib.js

--- week0/problems/solutions/fib.js
+++ week0/problems/solutions/fib.js
@@ -1,1 +1,18 @@
 // solution here
+'use strict';
+
+var fib = function (n) {
+    // 1st fibonacci number is 0
+    // 2nd fibonacci number is 1, etc...
+    var memo = [0, 0, 1],
+        nth_fib = function (m) {
+            if (memo[m] === undefined) {
+                memo[m] = nth_fib(m - 1) + nth_fib(m - 2);
+            }
+            return memo[m];
+        };
+    return nth_fib(n);
+};
+
+console.log('the 10th fibonacci number is: ' + fib(10));
+console.log('the 50th fibonacci number is: ' + fib(50));
