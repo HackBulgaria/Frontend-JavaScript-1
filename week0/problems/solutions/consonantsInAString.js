fd07c5f3d2dd6a1b47a0967fe96c7b119b85d822
dtaskoff <danko@abv.bg>
2014-06-08 19:40:15 +0300

    Solved consonantsInAString problem

--- week0/problems/solutions/consonantsInAString.js
+++ week0/problems/solutions/consonantsInAString.js
@@ -1,1 +1,13 @@
-// solution here
+'use strict';
+
+
+var consonantsInAString = function (str) {
+    var patt = /^[bcdfghjklmnpqrstvwxz]$/i;
+    return str.split('').filter(function (char) {
+        return patt.test(char);
+    }).length;
+};
+
+console.log('Theistareykjarbunga has ' +
+    consonantsInAString('Theistareykjarbunga') +
+    ' vowels in it');
