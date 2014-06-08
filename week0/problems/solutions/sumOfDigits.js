b8164e1c6aad1aab3975b3261a2ad6ef4d0a9092
dtaskoff <danko@abv.bg>
2014-06-08 17:16:20 +0300

    Solved sumOfDigits problem

--- week0/problems/solutions/sumOfDigits.js
+++ week0/problems/solutions/sumOfDigits.js
@@ -1,1 +1,18 @@
-// solution here
+'use strict';
+
+
+var sumOfDigits = function (number) {
+    var sum = 0;
+    // counts only digits before the floating point
+    number = Math.abs(number);
+
+    while (number > 0) {
+        sum += parseInt(number % 10, 10);
+        number = parseInt(number / 10, 10);
+    }
+    return sum;
+};
+
+console.log('sum of digits of 1234 is: ' + sumOfDigits(1234));
+console.log('sum of digits of -20794 is: ' + sumOfDigits(-20794));
+console.log('sum of digits of 42.42 is: ' + sumOfDigits(42.42));
