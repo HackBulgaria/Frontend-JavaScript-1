7b76ca4e56c43572cc5ff202a1e8eb91b72047c5
dtaskoff <danko@abv.bg>
2014-06-08 17:30:13 +0300

    sumOfMinAndMax problem solved

--- week0/problems/solutions/sumOfMinAndMax.js
+++ week0/problems/solutions/sumOfMinAndMax.js
@@ -1,1 +1,19 @@
-// solution here
+'use strict';
+
+// O(n)
+var sumOfMinAndMax = function (arr) {
+    var min = arr[0], max = min;
+
+    arr.forEach(function (n) {
+        if (n < min) {
+            min = n;
+        } else if (n > max) {
+            max = n;
+        }
+    });
+    // if arr is empty, min + max will be NaN,
+    //so it will return 0
+    return min + max || 0;
+};
+
+exports.sumOfMinAndMax = sumOfMinAndMax;
