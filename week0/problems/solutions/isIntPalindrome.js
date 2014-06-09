bb1bcc10535a5d9a792fbf3c58e3309e00f28a4a
dtaskoff <danko@abv.bg>
2014-06-09 17:05:25 +0300

    Added comments description to isIntPalindrome problem

--- week0/problems/solutions/isIntPalindrome.js
+++ week0/problems/solutions/isIntPalindrome.js
@@ -1,3 +1,8 @@
+/*
+    Solution by Daniel Taskoff
+    @github: https://github.com/dtaskoff/
+*/
+
 'use strict';
 
 
@@ -13,5 +18,7 @@ var isIntPalindrome = function (number) {
 
 console.log('123454321 is a palindrome? ' + isIntPalindrome(123454321));
 console.log('12345 is a palindrome? ' + isIntPalindrome(12345));
+console.log('323323 is a palindrome? ' + isIntPalindrome(323323));
+console.log('1984 is a palindrome? ' + isIntPalindrome(1984));
 
 exports.isIntPalindrome = isIntPalindrome;
