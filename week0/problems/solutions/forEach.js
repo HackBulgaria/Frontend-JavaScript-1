b0d37095d0d6f823d22d9a112894678eaf5d88f1
dtaskoff <danko@abv.bg>
2014-06-09 17:05:09 +0300

    Added comments description to forEach problem

--- week0/problems/solutions/forEach.js
+++ week0/problems/solutions/forEach.js
@@ -1,15 +1,22 @@
+/*
+    Solution by Daniel Taskoff
+    @github: https://github.com/dtaskoff/
+*/
+
 'use strict';
 
 
 var forEach = function (func, arr) {
+    var i = 0,
+        n = 0;
     if (!Array.isArray(arr)) {
         throw {
             name: 'TypeError',
             message: 'forEach expects an array as a second argument!'
         };
     }
-    var n = arr.length;
-    for (var i = 0; i < n; i += 1) {
+    n = arr.length;
+    for (i = 0; i < n; i += 1) {
         func(arr[i], i, arr);
     }
 };
