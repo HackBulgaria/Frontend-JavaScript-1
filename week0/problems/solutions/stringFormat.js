4cd0ef7bc0b94b89e02158198e639d3f4e2942f7
dtaskoff <danko@abv.bg>
2014-06-09 17:19:46 +0300

    Solved stringFormat

--- week0/problems/solutions/stringFormat.js
+++ week0/problems/solutions/stringFormat.js
@@ -1,1 +1,19 @@
-// solution here
+/*
+    Solution by Daniel Taskoff
+    @github: https://github.com/dtaskoff/
+*/
+
+'use strict';
+
+
+var stringFormat = function (string, placeholders) {
+    var result = string,
+        placeholder;
+    for (placeholder in placeholders) {
+        result = result.replace('{' + placeholder + '}',
+            placeholders[placeholder]);
+    }
+    return result;
+};
+
+exports.stringFormat = stringFormat;
