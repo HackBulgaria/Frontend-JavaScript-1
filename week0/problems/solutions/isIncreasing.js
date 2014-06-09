e93b3f43098917b7e0e2061dc4e1429a83d889d2
dtaskoff <danko@abv.bg>
2014-06-09 17:05:32 +0300

    Added comments description to isIncreasing problem

--- week0/problems/solutions/isIncreasing.js
+++ week0/problems/solutions/isIncreasing.js
@@ -1,12 +1,22 @@
+/*
+    Solution by Daniel Taskoff
+    @github: https://github.com/dtaskoff/
+*/
+
 'use strict';
 
 
 var isIncreasing = function (seq) {
+    // if prev is false than [..., prev] isn't increasing
+    // else, we check if [..., prev, curr] is increasing,
+    // i.e. prev < curr. Inductively, we check for [..., curr, next] and so on..
     return seq.reduce(function (prev, curr) {
         if (prev !== false && prev < curr) {
             return curr;
         }
         return false;
+    // if seq is increasing, seq.reduce() will return the last element,
+    // but we want a boolean to be returned
     }) && true;
 };
 
