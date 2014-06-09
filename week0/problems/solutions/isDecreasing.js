4fe5b362ea502f12e4edd3c84a346258b2a045b4
dtaskoff <danko@abv.bg>
2014-06-09 17:05:43 +0300

    Added comments description to isDecreasing problem

--- week0/problems/solutions/isDecreasing.js
+++ week0/problems/solutions/isDecreasing.js
@@ -1,12 +1,23 @@
+/*
+    Solution by Daniel Taskoff
+    @github: https://github.com/dtaskoff/
+*/
+
 'use strict';
 
 
 var isDecreasing = function (seq) {
+    // analogous to my isIncreasing solution, but this time we're going
+    // right to left, i.e. curr is false if [curr, ...] isn't decreasing
+    // else if curr is not false and prev < curr, than [prev, curr, ...]
+    // will be also decreasing, and so on..
     return seq.reduceRight(function (prev, curr) {
         if (prev !== false && prev < curr) {
             return curr;
         }
         return false;
+    // if seq is decreasing, seq.reduceRight will return the first element,
+    // but we want a boolean to be returned
     }) && true;
 };
 
