563fa3ff88f51e6d11374098bc0e9c4dc81f420f
Radoslav Georgiev <g2h.horde@gmail.com>
2014-07-03 13:22:39 +0300

    Adding Access-Control-Origin header to CRUD task

--- week4/6-Students-CRUD/server/server.js
+++ week4/6-Students-CRUD/server/server.js
@@ -14,6 +14,12 @@ var Student = mongoose.model("Student", {
   courses: [String]
 });
 
+app.all("*", function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", ["X-Requested-With", "Content-Type"]);
+  next();
+});
+
 app.use(bodyParser.json());
 
 // lists all students
